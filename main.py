f2d3e07f4928b9092c80cb6634c3baa4cb3889b6
SanSanech <nickalai.filatov@yandex.ru>
2025-09-08 11:31:35 +0300

    3_commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("wassup")
+class User:
+    
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        
+        
+    def add_to_txt(self):
+        with open("data.txt", "w") as file:
+            file.write(self.name)
+    
+    
+user_1 = User("Alex", "awdqw@yandex.ru")
+
+user_1.add_to_txt()
+
+print("dwqdq")
